4ca2c5780750b00b8a4ea5b9795708c1438b882d
Elizabethpv <48952600+Elizabethpv@users.noreply.github.com>
2021-03-22 17:15:22 +0530

    Add project files.

---
+++ NavigationApp/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NavigationApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult HomePage()
+        {
+            return View();
+        }
+        public ActionResult Service()
+        {
+            return View();
+        }
+        public ActionResult Products()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult ContactUs()
+        {
+            return View();
+        }
+        public ActionResult LoginPage()
+        {
+            return View();
+        }
+    }
+}
